492f1c70557e0b754dbe77dcd3a3707ab6584b8e
Daud-infra01 <mohddaud929@gmail.com>
2026-04-10 22:33:38 +0530

    day 3: CPU usage from /proc/stat — raw kernel counters

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::fs;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
+     let cpu = get_cpu_usage();
+println!("CPU Usage: {:.1}%", cpu);
     let proc = fs::read_dir("/proc").unwrap();
 
     for entry in proc {
@@ -28,5 +32,33 @@ fn get_field(contents: &str, field: &str) -> Option<String> {
             return Some(line.split_whitespace().skip(1).collect::<Vec<_>>().join(" "));
         }
     }
+
+
     None
+
 }
+fn read_cpu_stat() -> (u64, u64) {
+    let contents = fs::read_to_string("/proc/stat").unwrap();
+    let first_line = contents.lines().next().unwrap();
+    let nums: Vec<u64> = first_line
+        .split_whitespace()
+        .skip(1)
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let idle = nums[3] + nums[4]; // idle + iowait
+    let total: u64 = nums.iter().sum();
+    (idle, total)
+}
+
+fn get_cpu_usage() -> f64 {
+    let (idle1, total1) = read_cpu_stat();
+    thread::sleep(Duration::from_millis(500));
+    let (idle2, total2) = read_cpu_stat();
+
+    let idle_delta = idle2 - idle1;
+    let total_delta = total2 - total1;
+
+    (1.0 - idle_delta as f64 / total_delta as f64) * 100.0
+}
+
